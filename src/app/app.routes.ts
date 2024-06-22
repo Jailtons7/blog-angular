10462385da38e05b3d97a986f982ad9dc70cdebe
Jailton Silva <jss1@ifal.edu.br>
2024-06-21 22:25:37 -0300

    removing some default stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import { Routes, Route } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { PostsComponent } from './pages/posts/posts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'posts',
+        component: PostsComponent,
+      },
+    ],
+  },
+];
