ff3a21a029528b86a3e69333572b88ff0a01e3e7
Jailton Silva <jss1@ifal.edu.br>
2024-06-23 23:53:37 -0300

    add first version of login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginObj: Login;
 
+  constructor(private http: HttpClient, private router: Router) {
+    this.loginObj = new Login();
+  }
+
+  onLogin() {
+    const form = new FormData();
+    form.append('username', this.loginObj.username);
+    form.append('password', this.loginObj.password);
+    this.http.post('http://0.0.0.0:8000/auth/access-token', form).subscribe({
+      next: (res: any) => {
+        // set token to local storage and then navigate to the posts
+        localStorage.setItem('token', res.access_token);
+        this.router.navigateByUrl('/posts');
+      },
+      error: (err: any) => {
+        alert(err.error.detail);
+      },
+    });
+  }
+}
+
+export class Login {
+  username: string;
+  password: string;
+
+  constructor() {
+    this.username = '';
+    this.password = '';
+  }
 }
