ea5b151fc254fb9727195fcd2bfba4ffc3370fa4
Jailton Silva <jss1@ifal.edu.br>
2024-06-27 19:19:01 -0300

    removing debugger and unused import

--- src/app/pages/login/login.interceptor.ts
+++ src/app/pages/login/login.interceptor.ts
@@ -1,10 +1,8 @@
 import { inject } from '@angular/core';
-import { Location } from '@angular/common';
 import { HttpInterceptorFn } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 export const TokenInterceptable: HttpInterceptorFn = (req, next) => {
-  debugger;
   const router = inject(Router);
   const is_login_page = router.url === '/login';
   const token: string | null = localStorage.getItem('token');
